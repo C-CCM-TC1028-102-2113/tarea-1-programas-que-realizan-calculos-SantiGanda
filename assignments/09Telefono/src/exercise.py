c30af7778fbcc4c0cc542c8e388dffd4b4562d99
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-23 20:24:25 -0500

    Pide tres datos para dar el costo del plan

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
     #Leer los datos
+    print("Dame el número de mensajes")
+mnsj=int(input())
+print("Dame el número de megas")
+megas=float(input())
+print("Dame el número de minutos")
+minutos=int(input())
+cost=(mnsj*.80)+(megas*.80)+(minutos*.80)
+print("El costo mensual es de: "+str(cost))
+
     pass
 
 
