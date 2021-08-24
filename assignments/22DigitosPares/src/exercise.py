7d8b5e039c6ca6f3ae66bb5633c43b1133581858
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-23 21:30:50 -0500

    Revisa qué números son pares y cuales no

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,5 +1,29 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("Dame un número de cuatro dígitos")
+num=(input())
+
+if int(num[0])%2 == 0:
+    par1=int(1)
+else:
+    par1=int(0)
+if int(num[1])%2 == 0:
+    par2=int(1)
+else:
+    par2=int(0)
+if int(num[2])%2 == 0:
+    par3=int(1)
+else:
+    par3=int(0)
+if int(num[3])%2 == 0:
+    par4=int(1)
+else:
+    par4=int(0)
+
+pares=par1+par2+par3+par4
+
+print("La cantidad de dígitos pares es: "+str(pares))
+
     pass
 
 
