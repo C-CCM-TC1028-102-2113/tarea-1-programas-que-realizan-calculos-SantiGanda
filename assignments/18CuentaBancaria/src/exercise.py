7af32fd9dba5d60e66aea3ed0d5c21c803d53e45
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-23 20:44:09 -0500

    Pide varios datos para tener el saldo final en un banco mensualmente

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("Dame el saldo del mes anterior")
+anterior=float(input())
+print("Dame los ingresos")
+ingresos=float(input())
+print("Dame los egresos")
+egresos=float(input())
+print("Dame la cantidad de cheques expedidos")
+cheques=float(input())*13
+
+saldo1=anterior+ingresos-(egresos+cheques)
+saldo2=saldo1*.075
+saldo3=saldo1-saldo2
+
+print("El saldo final de la cuenta es de: "+str(saldo3))
+
     pass
 
 if __name__ == '__main__':
