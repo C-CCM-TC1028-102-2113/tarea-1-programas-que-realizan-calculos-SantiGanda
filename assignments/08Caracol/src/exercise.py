1ede162fd58c039350fc2c79bfb591acc1045cc9
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-23 20:18:39 -0500

    Pide los minutos para calcular cuanto avanza

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("Dime los minutos")
+    mins=float(input())*60
+    cm=mins*.57
+    print("Centímetros recorridos: "+str(cm))
     pass
 
 if __name__ == '__main__':
