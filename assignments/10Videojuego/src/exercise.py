9ad13d09aa2aaa369da60298d8849d1a0864b704
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-08-23 20:29:34 -0500

    Pide datos de juegos nuevos y usados para saber el costo total

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("Dame la cantidad de juegos nuevos")
+nuevos=int(input())
+print("Dame la cantidad de juegos usados")
+usados=int(input())
+total=(nuevos*1000)+(usados*350)
+print("El total de la compra es de: "+str(total))
+
    pass
 
 
